c63b5e53c39829a2cd74222eb43d07cebdef219b
Kouraazio <ilkka.pyykkonen@gmail.com>
2014-02-19 22:22:48 +0200

    Now possible to add/remove deck
    No DB connection though.

--- www/directives/playerinfo.js
+++ www/directives/playerinfo.js
@@ -9,8 +9,30 @@ playerinfo = (function () {
                     player: '=player'
                 },
                 link: function (scope, element, attribs) {
-                    scope.addDeck = function (player, deck) {
+                    
+                    scope.player.newDeck = {};
+                    scope.addDeckVisible = false;
+                    
+                    scope.addDeck = function (player) {
+                        var newDeck = { name: scope.player.newDeck.name, commander: scope.player.newDeck.commander };
                         
+                        scope.player.decks.push(newDeck);
+                        scope.player.newDeck.name = '';
+                        scope.player.newDeck.commander = '';
+                        
+                        scope.addDeckVisible = false;
+                    }
+                    scope.showAddDeck = function (){
+                        scope.addDeckVisible = true;
+                    }
+                    scope.hideAddDeck = function () {
+                        scope.addDeckVisible = false;
+                    }
+                    scope.deleteDeck = function (deck) {
+                        var index = scope.player.decks.indexOf(deck);
+                        if (index > -1) {
+                            scope.player.decks.splice(index, 1);
+                        }
                     }
                     
                 },
